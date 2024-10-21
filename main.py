bug fixed on kldiv loss computation, main loop completed

main.py
@@ -1,11 +1,9 @@
 # This is a sample Python script.
-import numpy as np
 import torch
 import torchmetrics
-from torch.utils.data import DataLoader
 from transformers import BertTokenizer, BertConfig
 
-from data.metrics.metric import acc_and_f1, kldiv_loss
+from data.metrics.metric import kldiv_loss, mae_loss, mse_loss
 from model.counting import ClassificationAndCounting
 from model.sequence_tagging import BertForSequenceTagging, BertForLabelDistribution
 from preprocessing.data import DataProcessor, load_examples
@@ -18,8 +16,6 @@ from train import train, evaluate
 
 
 def main():
-
-
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     dataprocessor = DataProcessor()
     data_dir = "./data/neoplasm"
@@ -45,27 +41,43 @@ def main():
         model_name_or_path,
         config=config
     )
-    model.to(device)
-    #wrapper = ClassificationAndCounting(learner=model, processor=dataprocessor)
-    #metrics = [torchmetrics.Accuracy(num_classes=3, average='macro', task = "multiclass"),
-    #       torchmetrics.F1Score(num_classes=3, average='macro', task = "multiclass")]
-    #history = train(device=device, train_dataset=train_ds, model=wrapper, eval_dataset=val_ds,
+    wrapper = ClassificationAndCounting(learner=model, processor=dataprocessor)
+    wrapper.to(device)
+    # metrics = [torchmetrics.Accuracy(num_classes=3, average='macro', task = "multiclass"),
+    #      torchmetrics.F1Score(num_classes=3, average='macro', task = "multiclass")]
+    # history = train(device=device, train_dataset=train_ds, model=wrapper, eval_dataset=val_ds,
     #                generate_preds_labels_fn=classification_preds_labels_fn, metrics=metrics)
-
-    # _ , test_metric = evaluate(device = device, model = model,eval_dataset= test_ds, eval_batch_size= 32)
+    # _ , test_metric = evaluate(device = device, model = model,eval_dataset= test_ds, eval_batch_size= 32,
+    # gen_preds_labels_fn=classification_preds_labels_fn)
     config.num_labels = 3
-    qua_model = BertForLabelDistribution.from_pretrained(model_name_or_path, config=config, loss_fn = kldiv_loss)
-    qua_model.to(device)
-    metrics = [torchmetrics.KLDivergence(log_prob=False, reduction="mean"),
-               torchmetrics.MeanAbsoluteError(),
-               torchmetrics.MeanSquaredError()]
-    train(device=device, train_dataset=train_ds, model=qua_model, eval_dataset=val_ds,
-                    generate_preds_labels_fn=proba_preds_labels_fn, metrics=metrics)
+    losses = [kldiv_loss, mae_loss, mse_loss]
+    models = [wrapper]
+    for loss in losses:
+        qua_model = BertForLabelDistribution.from_pretrained(model_name_or_path, config=config, loss_fn=loss)
+        qua_model.to(device)
+        models.append(qua_model)
+    histories = {}
+    test_metrics = {}
+    for model in models:
+        metrics = [torchmetrics.KLDivergence(log_prob=False, reduction="mean"),
+                   torchmetrics.MeanAbsoluteError(),
+                   torchmetrics.MeanSquaredError()]
+        history = train(device=device, train_dataset=train_ds, model=model, eval_dataset=val_ds,
+                        generate_preds_labels_fn=proba_preds_labels_fn, metrics=metrics)
+        histories[model.name] = history
+        test_metric = evaluate(device, model, test_ds, eval_batch_size= 32, metrics = metrics, gen_preds_labels_fn=proba_preds_labels_fn)
+        test_metrics[model.name] = test_metric
+
+
+
+
+
+
 
 
 def proba_preds_labels_fn(inputs, outputs):
     _, _, _, pred_proba = outputs
-    pred_proba = pred_proba.detach().cpu().numpy()
+    pred_proba = pred_proba.detach().cpu()
     labels_proba = inputs["labels_proba"]
     return pred_proba, labels_proba
 
@@ -82,7 +94,6 @@ def classification_preds_labels_fn(inputs, outputs):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    #print(torch.__version__)
     main()
 
 # QUINDI COME NEXT STEP HO:

train.py
@@ -20,27 +20,21 @@ def process_batch(batch, device, model, gen_preds_labels_fn, eval=False):
 def evaluate(device, model, eval_dataset, eval_batch_size, metrics, gen_preds_labels_fn):
     eval_dataloader = DataLoader(eval_dataset, batch_size=eval_batch_size)
     epoch_loss_sum = 0.0
-
     # Reset metrics before evaluation
     for metric in metrics:
         metric.reset()
-
     model.eval()
     with torch.no_grad():
         for batch in tqdm(eval_dataloader, desc="Evaluating"):
             outputs, preds, labels = process_batch(batch, device, model, gen_preds_labels_fn, eval=True)
             loss = outputs[0]
             epoch_loss_sum += loss.item()
-
             # Update each metric with current batch's predictions and labels
             for metric in metrics:
                 metric.update(preds, labels)
-
     eval_loss = epoch_loss_sum / len(eval_dataloader)
-
     # Compute all metrics
     eval_metrics = {metric.__class__.__name__: metric.compute().item() for metric in metrics}
-
     return eval_loss, eval_metrics
 
 
@@ -91,30 +85,19 @@ def train(
             loss = outputs[0]
             loss.backward()
             epoch_loss_sum += loss.item()
-            # Update metrics with current batch predictions and labels
             for metric in metrics:
                 metric.update(preds, labels)
-
             optimizer.step()
             scheduler.step()
             model.zero_grad()
         epoch_loss = epoch_loss_sum / num_steps_per_epoch
         history['train_loss'].append(epoch_loss)
-
-        # Retrieve metric values after the epoch
         epoch_metrics = {metric.__class__.__name__: metric.compute().item() for metric in metrics}
-
-        # Perform evaluation at the end of the epoch
-        eval_loss, eval_metrics = evaluate(device, model, eval_dataset, eval_batch_size, metrics)
-
-        # Append eval_loss to history
+        eval_loss, eval_metrics = evaluate(device, model, eval_dataset, eval_batch_size, metrics,
+                                           generate_preds_labels_fn)
         history['eval_loss'].append(eval_loss)
-
-        # Append eval_metrics to history
         for metric_name, metric_value in eval_metrics.items():
             history['eval_metrics'][metric_name].append(metric_value)
-
-        # Log epoch summary
         print(f"Epoch {epoch + 1}/{num_train_epochs}")
         print(f"Train Loss: {epoch_loss:.4f}")
         for metric_name, metric_value in epoch_metrics.items():
@@ -133,7 +116,6 @@ def print_epoch_summary(epoch, epoch_loss, epoch_metric, eval_loss, eval_metric)
     print("Training Metrics:")
     for key, value in epoch_metric.items():
         print(f"  {key} = {value:.4f}")
-
     print(f"\nValidation Loss: {eval_loss:.4f}")
     print("Validation Metrics:")
     for key, value in eval_metric.items():